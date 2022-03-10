6c760fc2079f786a4a27629c5078634c25ba23c6
Leandro Oliveira Pereira <leandrobhte1@hotmail.com>
2022-03-10 16:48:20 -0300

    inclusao de avaliacao de servico

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -5,6 +5,8 @@ import About from '../../views/screens/About'
 import Home from '../../views/screens/Home'
 import Search from '../../views/screens/Search'
 import Reports from '../../views/screens/Reports'
+import History from '../../views/screens/Historico'
+import Avaliacao from '../../views/screens/Avaliacao'
 import LoginCadastro from '../../views/screens/loginCadastro'
 import Param from '../../views/screens/Param'
 
@@ -18,6 +20,8 @@ const Content = props => (
             <Route path="/login-cadastro" element={<LoginCadastro />}></Route>
             <Route path="/search" element={<Search />}></Route>
             <Route path="/reports" element={<Reports />}></Route>
+            <Route path="/history" element={<History />}></Route>
+            <Route path="/avaliacao" element={<Avaliacao />}></Route>
             {/* <Route path="/param/:id" element={<Param />}></Route>
             <Route path="/about" element={<About />}></Route> */}
             <Route path="*" element={<Home />}></Route>
